eac48f47527a6cd0db62d781f942004ff0144d44
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2025-01-06 10:57:43 +0000

    Update outsystems-plugin-esim.js

--- www/outsystems-plugin-esim.js
+++ www/outsystems-plugin-esim.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.eSimAdd = function (success, error, activationCode) {
     exec(success, error, 'eSIM', 'eSimAdd', [activationCode]);
 };
+
+exports.isEnabled = function (success, error) {
+    exec(success, error, 'eSIM', 'isEnabled', []);
+};
